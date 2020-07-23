2edb645ae3c59c76f5c55a8b02bbbc5e8e27ec55
Paul Melekhov <gugglegum@gmail.com>
2020-07-23 23:27:53 +0500

    Improved translate of one phrase on profile page

--- app/I18n/Ru/Profile.php
+++ app/I18n/Ru/Profile.php
@@ -11,10 +11,9 @@ class Profile extends RussianLanguage
     // profile
     protected static $profile_page_title = 'Пользователь {username}';
     protected static $games_you_are_playing_title = 'Игры, в которых вы участвуете';
-    protected static $words_count = '{count} слов';
-//    protected static function words_count(array $values)
-//    {
-//        return $values['count'] . ' ' . self::plural($values['count'], 'слово', 'слова', 'слов');
-//    }
+    protected static function words_count(array $values)
+    {
+        return $values['count'] . ' ' . self::plural($values['count'], 'слово', 'слова', 'слов');
+    }
 
 }
